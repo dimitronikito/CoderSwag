9df9298b36d9a6fce999722d902072000aef5407
Dimitrios Nikitopoulos <dimitronikito@gmail.com>
2019-10-09 20:40:40 -0400

    lesson 6

--- app/src/main/java/com/diminiki/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/diminiki/coderswag/Controller/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.diminiki.coderswag.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.diminiki.coderswag.Adapters.CategoryRecycleAdapter
 import com.diminiki.coderswag.R
 import com.diminiki.coderswag.Services.DataService
+import com.diminiki.coderswag.Utilities.EXTRA_CATEGORY
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryRecycleAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories) { category ->
+            val productIntent = Intent(this, ProductsActivity::class.java)
+            productIntent .putExtra(EXTRA_CATEGORY, category.title)
+            startActivity(productIntent)
+        }
         categoryListView.adapter = adapter
 
         val layoutManager = LinearLayoutManager(this)
---
+++ app/src/main/java/com/diminiki/coderswag/Controller/ProductsActivity.kt
@@ -0,0 +1,17 @@
+package com.diminiki.coderswag.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diminiki.coderswag.R
+import com.diminiki.coderswag.Utilities.EXTRA_CATEGORY
+
+class ProductsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+
+        val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
+
+    }
+}
