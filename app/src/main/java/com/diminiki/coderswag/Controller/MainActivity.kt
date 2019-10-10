6aa138f1f30c66d0af5374e5606d26b036851303
Dimitrios Nikitopoulos <dimitronikito@gmail.com>
2019-10-09 20:00:54 -0400

    refactored for recycler view adapter

--- app/src/main/java/com/diminiki/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/diminiki/coderswag/Controller/MainActivity.kt
@@ -2,22 +2,25 @@ package com.diminiki.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.diminiki.coderswag.Adapters.CategoryAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.diminiki.coderswag.Adapters.CategoryRecycleAdapter
 import com.diminiki.coderswag.R
 import com.diminiki.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
 
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
